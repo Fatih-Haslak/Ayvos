f5fb7102e0195cb5b6c43a0cf1059fa75c920e25
Fatih HAŞLAK <62775596+Fatih-Haslak@users.noreply.github.com>
2023-07-14 20:05:45 +0300

    Add files via upload

--- car_counter.py
+++ car_counter.py
@@ -5,6 +5,8 @@ class CarCounter(ObjectDetector):
     def __init__(self,model_path):
         super().__init__(model_path)
         self.dedector=ObjectDetector(model_path)
+        self.sayac = 0
+        self.temp_liste = [0,1200]
 
     def string_parser(self,data):
         lines = data.strip().split("\n")
@@ -29,13 +31,35 @@ class CarCounter(ObjectDetector):
         return objects
 
     def frame_converter(self,frame):
-        pass
-
-    def outlier(self,data):
-        pass
+        cv2.line(frame,(183,477),(584,477),(0,100,244),2)
+        cv2.line(frame,(679,477),(1031,477),(100,50,100),2)
+        cv2.line(frame,(50,550),(550,550),(0,100,244),2)
+        cv2.line(frame,(670,348),(840,348),(100,50,100),2)
     
-    def counter(self,data):
-        pass
+    def import_area(self,data):
+        
+        orta_nokta_x=(data[0]+data[2])/2
+        orta_nokta_y=(data[1]+data[3])/2
+   
+        if(orta_nokta_x>50 and orta_nokta_y>477 and orta_nokta_x<584 and orta_nokta_y<550):
+            print("Anlık",orta_nokta_x,orta_nokta_y)
+            print("Eski", self.temp_liste[0],self.temp_liste[1])
+
+            self.temp_liste.clear()
+            self.temp_liste.append((data[0]+data[2]/2))
+            self.temp_liste.append((data[1]+data[3]/2))
+
+        else:
+            return 0
+    
+    def counter(self,data,count):
+        
+        for i in range(0,count):
+            if(self.import_area(data[i,:])):
+                self.sayac=self.sayac+1
+
+        print("Araç sayısı",self.sayac)    
+
 
     def run(self): #counbt için gerekli
         bouindig_boxes_liste=[]
@@ -43,7 +67,7 @@ class CarCounter(ObjectDetector):
         count=0
         while(1):
             veri,frame,fps = self.dedector.run()
-            
+            self.frame_converter(frame)
             liste=self.string_parser(veri)
             
             cv2.imshow("Dedect",frame)
@@ -57,16 +81,13 @@ class CarCounter(ObjectDetector):
                     arr[count:,2] = i[0]["x2"]
                     arr[count:,3] = i[0]["y2"]
                     count+=1
-                    # bouindig_boxes_liste.append(i[0]["x1"])
-                    # bouindig_boxes_liste.append(i[0]["y1"])
-                    # bouindig_boxes_liste.append(i[0]["x2"])
-                    # bouindig_boxes_liste.append(i[0]["y2"])
-            print(arr[0:count])
-            print("\n")
             print(veri)
-            print(count)
+            self.counter(arr,count)
             count=0
-    
+            print("Yeni veri")
+
+
+
 if __name__ == "__main__":
     count = CarCounter('/home/fatih/yolov7/yolov7.pt')
     count.run()
