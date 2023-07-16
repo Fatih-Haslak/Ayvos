d8e3779c5b6133cbed4dbd5bc829038ee595312a
Fatih HAŞLAK <62775596+Fatih-Haslak@users.noreply.github.com>
2023-07-16 19:26:41 +0300

    Add files via upload

--- car_counter.py
+++ car_counter.py
@@ -8,8 +8,7 @@ class CarCounter(ObjectDetector):
     def __init__(self,model_path):
         super().__init__(model_path)
         self.dedector=ObjectDetector(model_path)
-        self.sayac = 0
-        self.temp_liste = [[0,0]]
+        self.detect2_liste = []
         self.detect_liste = []
     
     def string_parser(self,data):
@@ -35,50 +34,48 @@ class CarCounter(ObjectDetector):
         return objects
 
     def frame_converter(self,frame):
-        cv2.line(frame,(183,477),(584,477),(0,100,244),2)
-        cv2.line(frame,(679,477),(1031,477),(100,50,100),2)
-        cv2.line(frame,(0,656),(550,656),(0,100,244),2)
-        cv2.line(frame,(670,348),(840,348),(100,50,100),2)
-    
-    def comp_list(self,liste1):
-        list_distance_indexer=[]
+        cv2.rectangle(frame, (0,400), (623,730), (255,124,0), 2)
+        cv2.rectangle(frame, (660,400), (1277,718), (0,25,255), 2)
+                             #x,y      #xmax y max
 
-        for sayac1,i in enumerate(liste1):
-            for sayac2, a in enumerate(liste1):
-                if(sayac1!=sayac2 and sayac1<sayac2):
-                    distance = math.sqrt((a[0] - i[0])**2 + (a[1] - i[1])**2)
-                    if distance<50:
-                        list_distance_indexer.append(sayac1)
-                    print("-----Yeni veri----")    
-                    print("Distance",distance)           
-        
-        
-        
-        for index in reversed(list_distance_indexer):
-            del liste1[index]
-
-        list_distance_indexer.clear()
-    
     def import_area(self,data):
         
-        orta_nokta_x=(data[0]+data[2])/2
-        orta_nokta_y=(data[1]+data[3])/2
+        orta_nokta_x= (data[0]+data[2])/2
+        orta_nokta_y= (data[1]+data[3])/2
         
-        #orta nokta sadece detect testı ıcın
-        
-        if(orta_nokta_x>0 and orta_nokta_y>477 and orta_nokta_x<584 and orta_nokta_y<656):
+    
+        if(orta_nokta_x>0 and orta_nokta_y>400 and orta_nokta_x<623 and orta_nokta_y<730): #zone 1 ıcınde mı
             self.detect_liste.append([orta_nokta_x,orta_nokta_y])
-            self.comp_list(self.detect_liste)
+        if(orta_nokta_x>660 and orta_nokta_y>400 and orta_nokta_x<1277 and orta_nokta_y<718):
+            self.detect2_liste.append([orta_nokta_x,orta_nokta_y])
 
-        
 
-    def counter(self,data,count):
+
+    def counter(self,data,count,frame):
         for i in range(0,count):
             self.import_area(data[i,:]) 
+         
+        
+        # print("Şerit gelen anlik araba sayisi",len(self.detect_liste))
+        # print("Şerit giden anlik araba sayisi",len(self.detect2_liste))
 
-        
-        print("Araba",len(self.detect_liste))
-        
+
+        frame = cv2.putText(frame, str(len(self.detect_liste)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1,  (0, 0, 0), 2, cv2.LINE_AA)
+
+        frame = cv2.putText(frame, "IN", (80,50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1,  (0, 0, 0), 2, cv2.LINE_AA)
+
+        frame = cv2.putText(frame, str(len(self.detect2_liste)), (700,50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1,  (255, 0, 0), 2, cv2.LINE_AA)
+
+        frame = cv2.putText(frame, "OUT", (800,50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1,  (255, 0, 0), 2, cv2.LINE_AA)
+    
+        self.detect_liste.clear()
+        self.detect2_liste.clear()
+
+        return frame
 
     def run(self): #count için gerekli
         bouindig_boxes_liste=[]
@@ -89,19 +86,23 @@ class CarCounter(ObjectDetector):
             self.frame_converter(frame)
             liste=self.string_parser(veri)
             
-            cv2.imshow("Detect",frame)
-            cv2.waitKey(fps)
+        
             #print(liste)
             # print("\n")
-            for i in liste:
-                if(i[0]["class_name"]=="car"):
-                    arr[count:,0] = i[0]["x1"]
-                    arr[count:,1] = i[0]["y1"]
-                    arr[count:,2] = i[0]["x2"]
-                    arr[count:,3] = i[0]["y2"]
-                    count+=1
-            self.counter(arr,count)
-            count=0
+            try:
+                for i in liste:
+                    if(i[0]["class_name"]=="car"):
+                        arr[count:,0] = i[0]["x1"]
+                        arr[count:,1] = i[0]["y1"]
+                        arr[count:,2] = i[0]["x2"]
+                        arr[count:,3] = i[0]["y2"]
+                        count+=1
+                frame=self.counter(arr,count,frame)
+                count=0
+                cv2.imshow("Detect",frame)
+                cv2.waitKey(fps)
+            except:
+                exit()
             # print("-----Yeni veri----")
 
 
