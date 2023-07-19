97aaa2d66c0bd7fca4aec05d8ad0a73f1f701129
Fatih HAŞLAK <62775596+Fatih-Haslak@users.noreply.github.com>
2023-07-19 12:25:34 +0300

    Add files via upload

--- detectron2/oop_detectron.py
+++ detectron2/oop_detectron.py
@@ -10,7 +10,8 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 import argparse
-
+from get_video import ThreadedCamera
+from detectron2.utils.visualizer import ColorMode
 
 #instances https://detectron2.readthedocs.io/en/latest/tutorials/models.html#model-output-format
 class Detector:
@@ -18,15 +19,16 @@ class Detector:
         setup_logger()
         sys.path.insert(0, os.path.abspath('./detectron2'))
         self.init_run_and_mode(mode)
+        self.camera=ThreadedCamera()
 
     def init_run_and_mode(self,select_mode):
 
         if(select_mode==1):
             #Dedection
-            path="COCO-Detection/faster_rcnn_R_50_C4_1x.yaml"
+            path="COCO-Detection/faster_rcnn_R_50_C4_3x.yaml"
         elif(select_mode==0):
-            #segment
-            path="COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+            #Mask
+            path="COCO-InstanceSegmentation/mask_rcnn_R_50_C4_3x.yaml"
         
         self.cfg = get_cfg()
         self.cfg.merge_from_file(model_zoo.get_config_file(path))
@@ -37,13 +39,23 @@ class Detector:
         
 
     def detect_objects(self, image_path):
-               
-        image = cv2.imread(image_path)
-        outputs = self.predictor(image)
-        visualizer = Visualizer(image[:, :, ::-1], self.metadata, scale=1.2)
-        out = visualizer.draw_instance_predictions(outputs["instances"].to("cpu"))
-        cv2.imshow("Result", out.get_image()[:, :, ::-1])
-        cv2.waitKey(0)
+
+        flag=0
+        while True:
+            try:
+                self.frame,self.FPS_MS = self.camera.show_frame()
+                flag=1
+            except AttributeError:
+                pass
+                
+            if flag==1:
+                flag=0       
+                image = self.frame
+                outputs = self.predictor(image)
+                visualizer = Visualizer(image[:, :, ::-1], self.metadata, scale=1.2,instance_mode=ColorMode.SEGMENTATION)
+                out = visualizer.draw_instance_predictions(outputs["instances"].to("cpu"))
+                cv2.imshow("Result", out.get_image()[:, :, ::-1])
+                cv2.waitKey(1)
         cv2.destroyAllWindows() 
     
 
@@ -51,8 +63,10 @@ class Detector:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description= ' Classification or Segmentation ')
-    parser.add_argument('deger', type=int, default=1, help=' 1 == Detection // 0 == Segmentasyon')
+    parser.add_argument('--deger', type=int, default=1, help=' 1 == Detection // 0 == Segmentasyon')
     args = parser.parse_args()
-    detector = Detector(args.deger)
+    deger=args.deger
+    print(deger)
+    detector = Detector(deger)
     detector.detect_objects("bus.jpg")
 
