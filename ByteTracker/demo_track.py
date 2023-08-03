83ca748bad8a982ecb6d25a5265113252bb06104
Fatih HAŞLAK <62775596+Fatih-Haslak@users.noreply.github.com>
2023-08-03 16:09:12 +0300

    add oop

--- ByteTracker/demo_track.py
+++ ByteTracker/demo_track.py
@@ -18,28 +18,151 @@ from yolox.tracker.byte_tracker import BYTETracker
 from yolox.tracking_utils.timer import Timer
 from yolov7.detect_oop import ObjectDetector
 
-
 IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
-detector=ObjectDetector()
+detector = ObjectDetector()
 
 
-def make_parser():
+class Predictor:
+    def __init__(self, exp, device=torch.device(0)):
+        self.num_classes = exp.num_classes
+        self.test_size = exp.test_size
+        self.device = device
+
+    def inference(self, img, timer):
+        img_info = {"id": 0}
+        if isinstance(img, str):
+            img_info["file_name"] = osp.basename(img)
+            img = cv2.imread(img)
+        else:
+            img_info["file_name"] = None
+        height, width = img.shape[:2]
+        img_info["height"] = height
+        img_info["width"] = width
+        img_info["raw_img"] = img
+        self.rgb_means = (0.485, 0.456, 0.406)
+        self.std = (0.229, 0.224, 0.225)
+
+        img, ratio = preproc(img, self.test_size, self.rgb_means, self.std)
+        img_info["ratio"] = ratio
+        img = torch.from_numpy(img).unsqueeze(0).float().to(self.device)
+
+        return img, img_info
+
+
+class ByteTrackDemo:
+    def __init__(self, args):
+        self.args = args
+        self.exp = get_exp(args.exp_file, args.name)
+        self.output_dir = osp.join(self.exp.output_dir, args.experiment_name)
+        os.makedirs(self.output_dir, exist_ok=True)
+        self.vis_folder = osp.join(self.output_dir, "track_vis")
+        os.makedirs(self.vis_folder, exist_ok=True)
+        self.device = torch.device("cuda" if args.device == "gpu" else "cpu")
+        self.predictor = Predictor(self.exp, self.device)
+        self.tracker = BYTETracker(args, frame_rate=args.fps)
+        self.vid_writer = None
+
+    def __get_image_list(self, path):
+        image_names = []
+        for maindir, subdir, file_name_list in os.walk(path):
+            for filename in file_name_list:
+                apath = osp.join(maindir, filename)
+                ext = osp.splitext(apath)[1]
+                if ext in IMAGE_EXT:
+                    image_names.append(apath)
+        return image_names
+
+    def __write_results(self, filename, results):
+        save_format = '{frame},{id},{x1},{y1},{w},{h},{s},-1,-1,-1\n'
+        with open(filename, 'w') as f:
+            for frame_id, tlwhs, track_ids, scores in results:
+                for tlwh, track_id, score in zip(tlwhs, track_ids, scores):
+                    if track_id < 0:
+                        continue
+                    x1, y1, w, h = tlwh
+                    line = save_format.format(frame=frame_id, id=track_id, x1=round(x1, 1),
+                                              y1=round(y1, 1), w=round(w, 1), h=round(h, 1), s=round(score, 2))
+                    f.write(line)
+
+    def imageflow_demo(self):
+        cap = cv2.VideoCapture(self.args.path if self.args.demo == "video" else self.args.camid)
+        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        timestamp = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+        save_folder = osp.join(self.vis_folder, timestamp)
+        os.makedirs(save_folder, exist_ok=True)
+        if self.args.demo == "video":
+            save_path = osp.join(save_folder, self.args.path.split("/")[-1])
+        else:
+            save_path = osp.join(save_folder, "camera.mp4")
+        self.vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height)))
+
+        timer = Timer()
+        frame_id = 0
+        results = []
+        while True:
+            ret_val, frame = cap.read()
+            if ret_val:
+                img, img_info = self.predictor.inference(frame, timer)
+                timer.tic()
+                with torch.no_grad():
+                    my_outputs = detector.run(frame)
+
+                if my_outputs[0] is not None:
+                    output_results = my_outputs.cpu().numpy()
+                    online_targets = self.tracker.update(output_results, [img_info["height"], img_info["width"]],
+                                                         self.exp.test_size)
+
+                    online_tlwhs = []
+                    online_ids = []
+                    online_scores = []
+                    for t in online_targets:
+                        tlwh = t.tlwh
+                        tid = t.track_id
+                        vertical = tlwh[2] / tlwh[3] > self.args.aspect_ratio_thresh
+                        if tlwh[2] * tlwh[3] > self.args.min_box_area and not vertical:
+                            online_tlwhs.append(tlwh)
+                            online_ids.append(tid)
+                            online_scores.append(t.score)
+                            results.append(
+                                f"{frame_id},{tid},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},{t.score:.2f},-1,-1,-1\n"
+                            )
+                    timer.toc()
+                    online_im = plot_tracking(
+                        img_info['raw_img'], online_tlwhs, online_ids, frame_id=frame_id + 1, fps=1. / timer.average_time
+                    )
+
+                else:
+                    timer.toc()
+                    online_im = img_info['raw_img']
+
+                if self.args.save_result:
+                    cv2.imshow("Sonuc", online_im)
+                    self.vid_writer.write(online_im)
+
+                ch = cv2.waitKey(1)
+                if ch == 27 or ch == ord("q") or ch == ord("Q"):
+                    break
+            else:
+                break
+            frame_id += 1
+
+        if self.args.save_result:
+            res_file = osp.join(self.vis_folder, f"{timestamp}.txt")
+            self.write_results(res_file, results)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser("ByteTrack Demo!")
-    parser.add_argument(
-        "demo", default="image", help="demo type, eg. image, video and webcam"
-    )
-    parser.add_argument("-expn", "--experiment-name", type=str, default=None)
+    parser.add_argument("demo", default="image", help="demo type, eg. image, video and webcam")
+    parser.add_argument("-expn", "--experiment-name", type=str, default="default_experiment",
+                        help="experiment name (provide a default value if not provided)")
     parser.add_argument("-n", "--name", type=str, default=None, help="model name")
-
-    parser.add_argument(
-        "--path", default="./videos/palace.mp4", help="path to images or video"
-    )
+    parser.add_argument("--path", default="./videos/palace.mp4", help="path to images or video")
     parser.add_argument("--camid", type=int, default=0, help="webcam demo camera id")
-    parser.add_argument(
-        "--save_result",
-        action="store_true",
-        help="whether to save the inference result of image/video",
-    )
+    parser.add_argument("--save_result", action="store_true",
+                        help="whether to save the inference result of image/video")
 
     # exp file
     parser.add_argument(
@@ -59,7 +182,6 @@ def make_parser():
     parser.add_argument("--tsize", default=None, type=int, help="test img size")
     parser.add_argument("--fps", default=30, type=int, help="frame rate (fps)")
 
-    
     # tracking args
     parser.add_argument("--track_thresh", type=float, default=0.5, help="tracking confidence threshold")
     parser.add_argument("--track_buffer", type=int, default=30, help="the frames for keep lost tracks")
@@ -70,170 +192,7 @@ def make_parser():
     )
     parser.add_argument('--min_box_area', type=float, default=10, help='filter out tiny boxes')
     parser.add_argument("--mot20", dest="mot20", default=False, action="store_true", help="test mot20.")
-    return parser
-
-
-def get_image_list(path):
-    image_names = []
-    for maindir, subdir, file_name_list in os.walk(path):
-        for filename in file_name_list:
-            apath = osp.join(maindir, filename)
-            ext = osp.splitext(apath)[1]
-            if ext in IMAGE_EXT:
-                image_names.append(apath)
-    return image_names
-
-
-def write_results(filename, results):
-    save_format = '{frame},{id},{x1},{y1},{w},{h},{s},-1,-1,-1\n'
-    with open(filename, 'w') as f:
-        for frame_id, tlwhs, track_ids, scores in results:
-            for tlwh, track_id, score in zip(tlwhs, track_ids, scores):
-                if track_id < 0:
-                    continue
-                x1, y1, w, h = tlwh
-                line = save_format.format(frame=frame_id, id=track_id, x1=round(x1, 1), y1=round(y1, 1), w=round(w, 1), h=round(h, 1), s=round(score, 2))
-                f.write(line)
-
-
-class Predictor(object):
-    def __init__(
-        self,
-        exp,
-        device=torch.device(0),
-
-    ):
     
-        self.num_classes = exp.num_classes
-        self.test_size = exp.test_size
-        self.device = device
-
-    def inference(self, img, timer):
-        img_info = {"id": 0}
-        if isinstance(img, str):
-            img_info["file_name"] = osp.basename(img)
-            img = cv2.imread(img)
-        else:
-            img_info["file_name"] = None
-        height, width = img.shape[:2]
-        img_info["height"] = height
-        img_info["width"] = width
-        img_info["raw_img"] = img
-        self.rgb_means = (0.485, 0.456, 0.406)
-        self.std = (0.229, 0.224, 0.225)
-
-
-        img, ratio = preproc(img, self.test_size, self.rgb_means, self.std)
-        img_info["ratio"] = ratio
-        img = torch.from_numpy(img).unsqueeze(0).float().to(self.device)
-            
-        return img,img_info
-
-def imageflow_demo(predictor, vis_folder, current_time, args):
-    cap = cv2.VideoCapture(args.path if args.demo == "video" else args.camid)
-    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    timestamp = time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
-    save_folder = osp.join(vis_folder, timestamp)
-    os.makedirs(save_folder, exist_ok=True)
-    if args.demo == "video":
-        save_path = osp.join(save_folder, args.path.split("/")[-1])
-    else:
-        save_path = osp.join(save_folder, "camera.mp4")
-    vid_writer = cv2.VideoWriter(
-        save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height))
-    )
-    tracker = BYTETracker(args, frame_rate=30)
-    timer = Timer()
-    frame_id = 0
-    results = []
-    while True:
-       
-        ret_val, frame = cap.read()
-        if ret_val:
-            img,img_info = predictor.inference(frame,timer)
-            timer.tic()
-            with torch.no_grad():
-                my_outputs= detector.run(frame)
-            #print("fato cıktılar",my_outputs)
-           
-            if my_outputs[0] is not None:
-                #print("NE bu",outputs[0].cpu().detach().numpy())
-                output_results = my_outputs.cpu().numpy()
-                #print(output_results[:,1])
-                online_targets = tracker.update(output_results, [img_info["height"], img_info["width"]], exp.test_size)
-                
-                online_tlwhs = []
-                online_ids = []
-                online_scores = []
-                for t in online_targets:
-                    tlwh = t.tlwh
-                    tid = t.track_id
-                    vertical = tlwh[2] / tlwh[3] > args.aspect_ratio_thresh
-                    if tlwh[2] * tlwh[3] > args.min_box_area and not vertical:
-                        online_tlwhs.append(tlwh)
-                        online_ids.append(tid)
-                        online_scores.append(t.score)
-                        results.append(
-                            f"{frame_id},{tid},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},{t.score:.2f},-1,-1,-1\n"
-                        )
-                timer.toc()
-                online_im = plot_tracking(
-                    img_info['raw_img'], online_tlwhs, online_ids, frame_id=frame_id + 1, fps=1. / timer.average_time
-                )
-                #cv2.imshow("sonuc",online_im)
-            
-            else:
-                timer.toc()
-                online_im = img_info['raw_img']
-            if args.save_result:
-                cv2.imshow("Sonuc",online_im)
-                vid_writer.write(online_im)
-            ch = cv2.waitKey(1)
-            if ch == 27 or ch == ord("q") or ch == ord("Q"):
-                break
-        else:
-            break
-        frame_id += 1
-
-    if args.save_result:
-        res_file = osp.join(vis_folder, f"{timestamp}.txt")
-        with open(res_file, 'w') as f:
-            f.writelines(results)
-       
-
-
-
-def main(exp, args):
-    if not args.experiment_name:
-        args.experiment_name = exp.exp_name
-
-    output_dir = osp.join(exp.output_dir, args.experiment_name)
-    os.makedirs(output_dir, exist_ok=True)
-
-    if args.save_result:
-        vis_folder = osp.join(output_dir, "track_vis")
-        os.makedirs(vis_folder, exist_ok=True)
-
-    args.device = torch.device("cuda" if args.device == "gpu" else "cpu")
-
-    print("args.device",args.device)
-
-    if args.tsize is not None:
-        exp.test_size = (args.tsize, args.tsize)
-
-
-    predictor = Predictor(exp, args.device)
-    current_time = time.localtime()
-    if args.demo == "image":
-        image_demo(predictor, vis_folder, current_time, args)
-    elif args.demo == "video" or args.demo == "webcam":
-        imageflow_demo(predictor, vis_folder, current_time, args)
-
-
-if __name__ == "__main__":
-    args = make_parser().parse_args()
-    exp = get_exp(args.exp_file, args.name)
-
-    main(exp, args)
+    args = parser.parse_args()
+    demo = ByteTrackDemo(args)
+    demo.imageflow_demo()
